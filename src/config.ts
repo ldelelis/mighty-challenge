be4cfab79505c9b9967d33ac0ac6cf0f4ba77b1b
Lucio Delelis <me@ldelelis.dev>
2021-08-26 01:47:02 -0300

    corrects postgres volume, defaults configurations

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
-export const SERVICE_PORT = process.env.SERVICE_PORT;
+export const SERVICE_PORT = process.env.SERVICE_PORT || 8000;
 export const PAGINATION_LIMIT = process.env.PAGINATION_LIMIT || 20;
